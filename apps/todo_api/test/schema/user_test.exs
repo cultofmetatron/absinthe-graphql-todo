9210c8731a9829e6c900f1a0aba5bc583ffdd094
Peter de Croos <cultofmetatron@aumlogic.com>
2017-04-11 15:42:07 -0400

    stubbed some schema tests

--- apps/todo_api/test/schema/user_test.exs
+++ apps/todo_api/test/schema/user_test.exs
@@ -4,12 +4,39 @@ defmodule LaVoyage.Db.UserTest do
   alias TodoApi.Web.TodoApi.Schema.User 
 
   @valid_attrs %{uid: "some content"}
+  @valid_signup_attrs %{
+    email: "foobar@example.com",
+    password: "foobar677"
+  }
   @invalid_attrs %{}
 
-  test "changeset with valid attributes" do
-    #changeset = User.changeset(%User{}, @valid_attrs)
-    #assert changeset.valid?
+  describe "signup changeset" do
+    test "works with valid arguments" do
+      changeset = User.signup_changeset(%User{}, @valid_signup_attrs)
+      assert changeset.valid?
+    end
+
+    test "requires a valid email" do
+      no_email = %{
+        email: "brad",
+        password: "asfga67585ASDF"
+      }
+      changeset = User.signup_changeset(%User{}, no_email)
+      assert !changeset.valid?
+    end
+
+    test "requires a password with appropriate properties" do
+      bad_pass = %{
+        email: "brad@example.com",
+        pass: foo
+      }
+      changeset = User.signup_changeset(%User{}, bad_pass)
+      assert !changeset.valid?
+    end
+
+
   end
+
 
   #test "changeset with invalid attributes" do
   #  changeset = User.changeset(%User{}, @invalid_attrs)
