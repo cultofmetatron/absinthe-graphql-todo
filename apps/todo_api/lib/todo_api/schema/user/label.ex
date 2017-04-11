8e22689a31a84d0fa443b20b67d0cac865630f78
Peter de Croos <cultofmetatron@aumlogic.com>
2017-04-11 19:22:30 -0400

    added some label tests

--- apps/todo_api/lib/todo_api/schema/user/label.ex
+++ apps/todo_api/lib/todo_api/schema/user/label.ex
@@ -20,8 +20,12 @@ defmodule TodoApi.Schema.User.Label do
     |> validate_required([:text])
   end
 
-  def add_changeset(%User{}=user, %Todo{}=todo, params \\ %{}) do
-    
+  def create_changeset(%User{}=user, %Todo{}=todo, params \\ %{}) do
+    build_assoc(user, :labels)
+      |> cast(params, [:text])
+      |> put_assoc(:todo, todo)
+      |> validate_required([:text])
+      |> validate_length(:text, min: 2)
   end
 
 
---
+++ apps/todo_api/test/schema/user/label_test.exs
@@ -0,0 +1,46 @@
+defmodule TodoApi.Test.Schema.LabelTest do
+  use TodoApi.DataCase
+
+  alias TodoApi.Schema.User
+  alias TodoApi.Schema.User.Todo
+  alias TodoApi.Schema.User.Label
+  alias TodoApi.Web
+  alias TodoApi.Repo
+
+  setup do
+    user_changeset = User.signup_changeset(%User{}, %{
+      email: "foobar@example.com",
+      password: "foobar677",
+      password_confirmation: "foobar677"
+    });
+    {:ok, user} = user_changeset |> Repo.insert()
+    {:ok, todo} = Todo.create_changeset(user, %{
+      content: "here is a test label"
+    }) |> Repo.insert()
+
+
+    %{user: user, todo: todo}
+  end
+
+  describe "creating a todo" do
+    test "we can apply a label", %{todo: todo, user: user} do
+      changeset = Label.create_changeset(user, todo, %{
+        text: "ourlabel"
+      });
+      
+      assert changeset.valid?
+    end
+
+    test "label must have more than 2 charachters", %{todo: todo, user: user} do
+      changeset = Label.create_changeset(user, todo, %{
+        text: "o"
+      });
+      
+      refute changeset.valid?
+    end
+
+
+  end
+
+ 
+end
