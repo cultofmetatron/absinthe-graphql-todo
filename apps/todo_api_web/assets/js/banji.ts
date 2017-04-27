e8e53c218fbfc130e6052518c728dab992fea027
Peter de Croos <cultofmetatron@aumlogic.com>
2017-04-27 13:09:18 -0400

    updated to typescript 2.3

--- apps/todo_api_web/assets/js/banji.ts
+++ apps/todo_api_web/assets/js/banji.ts
@@ -1,7 +1,7 @@
 
 //import {foobar2} from './tester';
 //const foobar = () => console.log("hello banji");
-import {foobar2} from 'tester';
+import {foobar2} from 'components/tester';
 
 
 export default foobar2;
--- apps/todo_api_web/assets/js/components/tester.tsx
+++ apps/todo_api_web/assets/js/components/tester.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
+import {Component} from 'react';
 import * as ReactDom from 'react-dom';
 //import {foobar2} from './tester';
 
 export interface TesterProps {}
 
-class foobar2 extends React.Component<TesterProps, undefined> {
+class foobar2 extends Component<TesterProps, undefined> {
   render() {
-    return <h1>hello world</h1>;
+    return <h1>hello Sam</h1>;
   }
 }
 
