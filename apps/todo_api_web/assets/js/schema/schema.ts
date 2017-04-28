d26c28695b19637ca71bf796c9cb7fc4aad795ed
Peter de Croos <cultofmetatron@aumlogic.com>
2017-04-28 18:00:01 -0400

    added signin query

--- apps/todo_api_web/assets/js/schema/schema.ts
+++ apps/todo_api_web/assets/js/schema/schema.ts
@@ -1,6 +1,20 @@
 //  This file was automatically generated and should not be edited.
 /* tslint:disable */
 
+export interface SigninMutationVariables {
+  email: string;
+  password: string;
+}
+
+export interface SigninMutation {
+  // sign the user in
+  signin: {
+    id: string | null,
+    email: string | null,
+    jwt: string | null,
+  } | null;
+}
+
 export interface SignupMutationVariables {
   email: string;
   password: string;
